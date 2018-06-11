e043ad74767aa39edc4ae0725617f3be4495c6fe
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2018-06-11 10:51:46 -0400

    show spinner while loading courses, cleanup style

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -14,9 +14,6 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 // external-global styles must be imported in your JS.
 import normalizeCss from 'normalize.css';
 import s from './Layout.css';
-import Header from '../Header';
-import Feedback from '../Feedback';
-import Footer from '../Footer';
 
 class Layout extends React.Component {
   static propTypes = {
@@ -24,14 +21,7 @@ class Layout extends React.Component {
   };
 
   render() {
-    return (
-      <div>
-        <Header />
-        {this.props.children}
-        <Feedback />
-        <Footer />
-      </div>
-    );
+    return <div>{this.props.children}</div>;
   }
 }
 
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -12,6 +12,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import { setCrosslistTarget } from '../../actions/crosslist';
+import Spinner from '../../components/Spinner';
 import s from './Home.css';
 
 class Home extends React.Component {
@@ -31,33 +32,41 @@ class Home extends React.Component {
   };
 
   render() {
+    const courses =
+      this.props.courses.length === 0 ? (
+        <Spinner />
+      ) : (
+        <form onSubmit={setCrosslistTarget}>
+          {this.props.courses.map(term => (
+            <div key={term.term.id}>
+              <h1 className={s.termTitle}>{term.term.name}</h1>
+              {term.courses.map(course => (
+                <div key={course.id} className={s.course}>
+                  <input
+                    type="radio"
+                    name="xlist-target"
+                    value={course.id}
+                    id={course.id}
+                  />
+                  <label htmlFor={course.id} className={s.courseInfo}>
+                    <h2 className={s.courseTitle}>{course.name}</h2>
+                    <div className={s.courseDesc}>
+                      {course.course_code} - {course.sis_course_id}
+                    </div>
+                  </label>
+                </div>
+              ))}
+            </div>
+          ))}
+          <button type="submit">Next</button>
+        </form>
+      );
+
     return (
       <div className={s.root}>
         <div className={s.container}>
           <h1>Select the course you would like to crosslist sections into</h1>
-          <form onSubmit={setCrosslistTarget}>
-            {this.props.courses.map(term => (
-              <div key={term.term.id}>
-                <h1>{term.term.name}</h1>
-                {term.courses.map(course => (
-                  <React.Fragment key={course.id}>
-                    <input
-                      type="radio"
-                      name="xlist-target"
-                      value={course.id}
-                      id={course.id}
-                    />
-                    <label htmlFor={course.id} className={s.newsItem}>
-                      <h2 className={s.newsTitle}>{course.name}</h2>
-                      <div className={s.newsDesc}>{course.course_code}</div>
-                      <div className={s.newsDesc}>{course.sis_course_id}</div>
-                    </label>
-                  </React.Fragment>
-                ))}
-              </div>
-            ))}
-            <button type="submit">Next</button>
-          </form>
+          {courses}
         </div>
       </div>
     );
